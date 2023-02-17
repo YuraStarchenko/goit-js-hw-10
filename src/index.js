1dbad6bbb71c9313671d110057d0f46c2b2eef14
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-17 12:51:48 +0200

    +

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ const refs = {
 	textInput: document.getElementById('search-box'),
 };
 
+function fetchCountries(name){
+	fetch('https://restcountries.com/v3.1/name')
+	.then(response => {
+		return response.json();
+	})
+	.then(response => {
+		console.log(name);
+	})
+	.catch(error => {
+		console.log(error);
+	})
+}
