4995577aa5649929218af78e11768a5c5d4a3a44
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-18 16:09:22 +0200

    +

--- src/index.js
+++ src/index.js
@@ -2,28 +2,40 @@ import './css/styles.css';
 import { fetchCountries } from './service/fetchCountries';
 import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
 
-const refs = {
-  searchBox: document.getElementById('search-box'),
-  countryList: document.querySelector('.country-list'),
-	countryInfo: document.querySelector('.country-info'),
-};
+// const refs = {
+//   searchBox: document.getElementById('search-box'),
+//   countryList: document.querySelector('.country-list'),
+// 	countryInfo: document.querySelector('.country-info'),
+// };
 
-refs.searchBox.addEventListener(
-	'input', 
-	debounce(() => {
-	const country = refs.searchBox.value;
-	fetchCountries(country).then(onShowCountry);
-	}, DEBOUNCE_DELAY)
-);
+// refs.searchBox.addEventListener(
+// 	'input', 
+// 	debounce(() => {
+// 	const country = refs.searchBox.value;
+// 	fetchCountries(country).then(onShowCountry);
+// 	}, DEBOUNCE_DELAY)
+// );
 
-function onShowCountry({
-	name,
-	capital,
-	population,
-	flags,
-	languages
-}) {
-	
-}
+// function onShowCountry({
+// 	name,
+// 	capital,
+// 	population,
+// 	flags,
+// 	languages
+// }) {
+// 	const countryInfo = `<div class="country">
+// 	<img src="${flags}" class="flags-country" alt="flags" width="30">
+// 		<div class="information">
+// 			<h1 class="name">${name}</h1>
+// 		</div>
+// 		<ul class="population">
+// 			<li>capital: <span>${capital}</span></li>
+// 			<li>population: <span>${population}</span></li>
+// 			<li>languages: <span>${languages}</span></li>
+// 		</ul>
+// 	</div>`
+
+// 	console.log(countryInfo);
+// }
--- src/service/fetchCountries.js
+++ src/service/fetchCountries.js
@@ -1,10 +1,13 @@
-const URL = 'https://restcountries.com/v3.1/name/';
+// const URL = 'https://restcountries.com/v3.1/name/';
 
-export function fetchCountries(name){
-	fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`).then(response => {
-		if(!response.ok){
-			throw new Error('Data fail!');
-		}
-		return response.json();
-	});
-}
+fetch('https://restcountries.com/v3.1/name');
+then(res => res.json());
+
+// export function fetchCountries(name){
+// 	fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`).then(response => {
+// 		if(!response.ok){
+// 			throw new Error('Sorry no country with that name!');
+// 		}
+// 		return response.json();
+// 	});
+// }
